f3f17768dc47da09b3465c8a21153f3ed7945cf6
Weston Steimel <weston.steimel@gmail.com>
2021-07-31 01:47:37 +0100

    split update scripts between stable and edge and don't commit until publish is successful
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- scripts/update-stable.sh
+++ scripts/update-stable.sh
@@ -17,15 +17,3 @@ sed -ri \
 git add stable/Dockerfile
 git diff-index --quiet HEAD || git commit --message "updated stable to version ${latest_stable_version}, revision ${latest_stable_revision}"
 
-edge_version="master"
-edge_revision=`curl --silent "https://api.github.com/repos/curl/curl/commits/${edge_version}" | jq .sha | xargs`
-echo "latest edge version: ${edge_version}, revision: ${edge_revision}"
-
-sed -ri \
-    -e 's/^(ARG VERSION=).*/\1'"\"${edge_version}\""'/' \
-    -e 's/^(ARG REVISION=).*/\1'"\"${edge_revision}\""'/' \
-    "edge/Dockerfile"
-
-git add edge/Dockerfile
-git diff-index --quiet HEAD || git commit --message "updated edge to version ${edge_version}, revision: ${edge_revision}"
-
