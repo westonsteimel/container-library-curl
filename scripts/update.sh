e60e815c3771f5e7c8dc9254581e1bcc2c2c24b5
Weston Steimel <weston.steimel@gmail.com>
2021-08-12 10:58:35 +0100

    update to github actions to perform all updates before building images
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

---
+++ scripts/update.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+latest_stable_release=`curl --silent "https://api.github.com/repos/curl/curl/releases/latest" | jq .tag_name | xargs`
+revision=`curl --silent "https://api.github.com/repos/curl/curl/commits/${latest_stable_release}" | jq .sha | xargs`
+version=${latest_stable_release#"curl-"}
+version=${version//_/.}
+
+echo "latest stable version: ${version}, revision: ${revision}"
+
+sed -ri \
+    -e 's/^(ARG VERSION=).*/\1'"\"${version}\""'/' \
+    -e 's/^(ARG REVISION=).*/\1'"\"${revision}\""'/' \
+    "stable/Dockerfile"
+
+git add stable/Dockerfile
+git diff-index --quiet HEAD || git commit --message "updated stable to version ${version}, revision ${revision}"
+
+version="master"
+revision=`curl --silent "https://api.github.com/repos/curl/curl/commits/${version}" | jq .sha | xargs`
+echo "latest edge version: ${version}, revision: ${revision}"
+
+sed -ri \
+    -e 's/^(ARG VERSION=).*/\1'"\"${version}\""'/' \
+    -e 's/^(ARG REVISION=).*/\1'"\"${revision}\""'/' \
+    "edge/Dockerfile"
+
+git add edge/Dockerfile
+git diff-index --quiet HEAD || git commit --message "updated edge to version ${version}, revision: ${revision}"
+
